0ea6485663276cd5371bfd7b4e13ecafd30a14ad
EC2 Default User <ec2-user@ip-172-31-24-40.us-east-2.compute.internal>
2019-11-04 08:30:43 +0000

    13 ニュース投稿画面を作成しよう

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,16 +1,49 @@
-{{-- layouts/profile.blade.phpを読み込む --}}
 @extends('layouts.profile')
-
-
-{{-- profile.blade.phpの@yield('title')に'プロフィールの新規作成'を埋め込む --}}
 @section('title', 'プロフィールの新規作成')
 
-{{-- profile.blade.phpの@yield('content')に以下のタグを埋め込む --}}
 @section('content')
     <div class="container">
         <div class="row">
             <div class="col-md-8 mx-auto">
                 <h2>プロフィール新規作成</h2>
+                <form action="{{ action('Admin\ProfileController@create') }}" method="post">
+
+                    @if (count($errors) > 0)
+                        <ul>
+                            @foreach($errors->all() as $e)
+                                <li>{{ $e }}</li>
+                            @endforeach
+                        </ul>
+                    @endif
+                    <div class="form-group row">
+                        <label class="col-md-2">氏名</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="name" value="{{ old('name') }}">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2">性別</label>
+                        <div class="col-md-10">
+                            <label class="inline"><input type="radio" name="gender" value="男性"> 男性</label>
+                            <label class="inline"><input type="radio" name="gender" value="女性"> 女性</label>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2">趣味</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="hobby" value="{{ old('hobby') }}">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2">自己紹介欄</label>
+                        <div class="col-md-10">
+                            <textarea class="form-control" name="introduction" rows="10">{{ old('introduction') }}</textarea>
+                        </div>
+                    </div>
+                    
+                    {{ csrf_field() }}
+                    <input type="submit" class="btn btn-primary" value="送信">
+                </form>
             </div>
         </div>
     </div>
