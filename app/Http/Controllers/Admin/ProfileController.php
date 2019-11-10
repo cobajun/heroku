185772c357a3d194b3c588751db7b9c107b5b85c
EC2 Default User <ec2-user@ip-172-31-24-40.us-east-2.compute.internal>
2019-11-10 02:15:11 +0000

    16 投稿したニュースを更新/削除しよう

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -12,8 +12,8 @@ class ProfileController extends Controller
         return view('admin.profile.create');
     }
 
-    public function create(Request $request)
-    {
+  public function create(Request $request)
+  {
       // Varidationを行う
       $this->validate($request, Profile::$rules);
 
@@ -28,15 +28,44 @@ class ProfileController extends Controller
       $profile->save();
       
         return redirect('admin/profile/create');
-    }
+  }
+  public function index(Request $request)
+  {
+      $cond_name = $request->cond_name;
+      if ($cond_name != '') {
+          // 検索されたら検索結果を取得する
+          $posts = Profile::where('title',$cond_name)->get();
+      } else {
+          // それ以外はすべてのニュースを取得する
+          $posts = Profile::all();
+      }
+      return view('admin.profile.index', ['posts' => $posts, 'cond_name' => $cond_name]);
+  }
+  
+  public function edit(Request $request)
+  {
+    return view('admin.profile.edit');
+    //return redirect('admin/profile/edit');
+    //$profile = Profile::find($request->id);
 
-    public function edit()
-    {
-        return view('admin.profile.edit');
-    }
+    //return view('admin.profile.edit', ['profile_form' => $profile]);
+  }
 
-    public function update()
-    {
-        return redirect('admin/profile/edit');
-    }
+  public function update(Request $request)
+  {
+      $this->validate($request, Profile::$rules);
+      $profile = Profile::find($request->id);
+      $profile_form = $request->all();
+      unset($profile_form['_token']);
+      $profile->fill($profile_form)->save();
+      
+      return redirect('admin/profile/edit');
+  }
+  
+  public function delete(Request $request)
+  {
+      $profile = Profile::find($request->id);
+      $profile->delete();
+      return redirect('admin/profile/');
+  }  
 }
